844249ad9c0436a9ec334bdd50f9b2f73ad99a10
Nicolas Petruci Penga <103520454+NicolasPetruci@users.noreply.github.com>
2023-07-17 17:18:20 -0300

    correções2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CursoModule } from './curso/curso.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CursoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'crudcursos',
+      entities:[
+        __dirname + '**/*.entity.{.ts,.js}',
+      ],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    CursoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
