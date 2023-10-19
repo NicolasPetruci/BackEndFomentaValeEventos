491dad9bf84d3f140229642cd7f3f8c264c351ac
Nicolas Petruci Penga <103520454+NicolasPetruci@users.noreply.github.com>
2023-10-19 14:37:53 -0300

    correção simples que faltava uma interrogação

--- src/curso/dto/create-curso.dto.ts
+++ src/curso/dto/create-curso.dto.ts
@@ -2,7 +2,7 @@ import { Palestrante, TipoEvento } from '@prisma/client';
 
 export class CreateCursoDto {
   nomeCurso: string;
-  dataCurso: string;
+  dataCurso?: string;
   tempoInicio?: Date | string;
   tempoFinalizacao?: Date | string;
   palestrante: Palestrante[];
